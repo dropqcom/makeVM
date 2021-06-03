2548c6d256af896e9a7bdf0bd97ee63445e185d6
dropqcom <fxscholler@protonmail.com>
2021-06-03 15:36:58 +0200

    again minor fixes

--- modules/ressources/index.php
+++ modules/ressources/index.php
@@ -2,6 +2,7 @@
 <html>
 <body>
 	<p> Test page, this should be the home page / landing page </p>
+	<p> http://127.0.0.1/lfi.php </p>
 </body>
 </html>
 
--- modules/ressources/lfi.php
+++ modules/ressources/lfi.php
@@ -3,8 +3,8 @@
 <body>
 	<h1> LFI Test page </h1>
 		<p> check if local file inclusion works </p>
-		<p> usage: http://127.0.0.1/lfi/lfi.php?file=/../../../etc/passwd </p>
-		<p> apache log poisoning see exploit.py in /lfi </p>
+		<p> usage: http://127.0.0.1:8800/lfi.php?file=/../../../etc/passwd </p>
+		<p> apache log poisoning see exploit.py </p>
 
 	<?php
 		$file = $_GET['file'];
